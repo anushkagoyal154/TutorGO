7378aacb69a5800d8206c304f83a4808b88001c6
Jahnvi <jahnvibhandari2006@gmail.com>
2026-09-07 08:08:26 +0530

    basic tutorgo  prototype

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,30 @@
-const mysql=require("mysql2");
+require("dotenv").config();
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"jahnvi_bhandari",
-    database:"TutorGo"
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-db.connect((err)=>{
-    if(err){
-        console.error("MySQL connection failed:",err);
+db.connect((err) => {
+
+    if (err) {
+
+        console.error(
+            "MySQL connection failed:",
+            err
+        );
+
         return;
     }
-    console.log("Connected to MySQL database");
+
+    console.log(
+        "Connected to MySQL database"
+    );
+
 });
 
-module.exports=db;
+module.exports = db;
